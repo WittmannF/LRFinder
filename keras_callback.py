e4bdf18821ba7ba34f209c3730f4268cedd62961
WittmannF <fernando.wittmann@gmail.com>
2019-07-11 17:27:24 -0300

    Update keras_callback.py

--- keras_callback.py
+++ keras_callback.py
@@ -4,10 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class LRFinder(Callback):
-    def __init__(self, min_lr, max_lr, mom=0.9):
+    def __init__(self, min_lr, max_lr, mom=0.9, stop_multiplier=None):
         self.min_lr = min_lr
         self.max_lr = max_lr
         self.mom = mom
+        if stop_multiplier is None:
+            self.stop_multiplier = -20*self.mom/3 + 10 # 4 if mom=0.9, 10 if mom=0
+        else:
+            self.stop_multiplier = stop_multiplier
         
     def on_train_begin(self, logs={}):
         n_iterations = self.params['samples']//self.params['batch_size']
@@ -32,7 +36,7 @@ class LRFinder(Callback):
         
         self.losses.append(loss)            
         self.iteration += 1
-        if loss > self.best_loss*4: # Stop criteria
+        if loss > self.best_loss*self.stop_multiplier: # Stop criteria
             self.model.stop_training = True
     
     def on_train_end(self, logs=None):
