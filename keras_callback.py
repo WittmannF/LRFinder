ef071844b2463f24e6fad5a0e3dfb23ca0ce64a7
WittmannF <fernando.wittmann@gmail.com>
2019-07-12 17:52:16 -0300

    Update keras_callback.py

--- keras_callback.py
+++ keras_callback.py
@@ -4,26 +4,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class LRFinder(Callback):
-    def __init__(self, min_lr, max_lr, mom=0.9, stop_multiplier=None):
+    def __init__(self, min_lr, max_lr, mom=0.9, stop_multiplier=None, 
+                 reload_weights=True, batches_lr_update=5):
         self.min_lr = min_lr
         self.max_lr = max_lr
         self.mom = mom
+        self.reload_weights = reload_weights
+        self.batches_lr_update = batches_lr_update
         if stop_multiplier is None:
-            self.stop_multiplier = -20*self.mom/3 + 10 # 4 if mom=0.9, 10 if mom=0
+            self.stop_multiplier = -20*self.mom/3 + 10 # 4 if mom=0.9
+                                                       # 10 if mom=0
         else:
             self.stop_multiplier = stop_multiplier
         
     def on_train_begin(self, logs={}):
+        p = self.params
         try:
-            n_iterations = self.params['epochs']*self.params['samples']//self.params['batch_size']
+            n_iterations = p['epochs']*p['samples']//p['batch_size']
         except:
-            n_iterations = self.params['steps']*self.params['epochs']
+            n_iterations = p['steps']*p['epochs']
             
         self.learning_rates = np.geomspace(self.min_lr, self.max_lr, \
-                                           num=n_iterations+1)
+                                           num=n_iterations//self.batches_lr_update+1)
         self.losses=[]
         self.iteration=0
         self.best_loss=0
+        if self.reload_weights:
+            self.model.save_weights('tmp.hdf5')
         
     
     def on_batch_end(self, batch, logs={}):
@@ -31,17 +38,24 @@ class LRFinder(Callback):
         
         if self.iteration!=0: # Make loss smoother using momentum
             loss = self.losses[-1]*self.mom+loss*(1-self.mom)
-
-        if self.iteration==0 or loss < self.best_loss: 
-            self.best_loss = loss
-            
-        lr = self.learning_rates[self.iteration]
-        K.set_value(self.model.optimizer.lr, lr)
         
-        self.losses.append(loss)            
-        self.iteration += 1
+        if self.iteration==0 or loss < self.best_loss: 
+                self.best_loss = loss
+                
+        if self.iteration%self.batches_lr_update==0: # Evaluate each lr over 5 epochs
+            
+            if self.reload_weights:
+                self.model.load_weights('tmp.hdf5')
+          
+            lr = self.learning_rates[self.iteration//self.batches_lr_update]            
+            K.set_value(self.model.optimizer.lr, lr)
+
+            self.losses.append(loss)            
+
         if loss > self.best_loss*self.stop_multiplier: # Stop criteria
             self.model.stop_training = True
+                
+        self.iteration += 1
     
     def on_train_end(self, logs=None):
         plt.figure(figsize=(12, 6))
